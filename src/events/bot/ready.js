e98903a576089fb60d6ec17ba3b38968f78607be
Daniel Alexssander <danielrossinatti15@gmail.com>
2023-06-02 12:59:37 -0300

    Update ready.js

--- src/events/bot/ready.js
+++ src/events/bot/ready.js
@@ -3,6 +3,7 @@ module.exports = async (client) => {
 		'Abra um ticket e faça seu orçamento agora mesmo!✅',
 	];
 	i = 0;
+	client.user.setActivity(status[0]);
 	setInterval(function () {
 		client.user.setActivity(status[0]);
 	}, 1 * 10000);
